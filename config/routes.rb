dd20a7bdf7260227365120e1ba9b67d423b31560
RisaOshima <107258614+Roshima5@users.noreply.github.com>
2022-09-07 16:20:27 +0000

    [add]各ページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  
+
   root to: "homes#top"
+  get "homes/about" => "homes#about", as: "about"
   resources :book, only: [:new, :index, :show, :edit]
   resources :user, only: [:index, :show, :edit]
   devise_for :users
