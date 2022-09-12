90f180b6e093fbe8c1b3b3dd2aa35011490376ad
RisaOshima <107258614+Roshima5@users.noreply.github.com>
2022-09-12 11:55:40 +0000

    [add]

--- app/controllers/book_controller.rb
+++ app/controllers/book_controller.rb
@@ -4,7 +4,7 @@ class BookController < ApplicationController
   end
 
   def index
-    @
+    @book = Book.new
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
 
   root to: "homes#top"
   get "homes/about" => "homes#about", as: "about"
-  resources :books, only: [:new, :index, :show, :edit]
-  resources :users, only: [:index, :show, :edit]
+  resources :book, only: [:new, :index, :show, :edit]
+  resources :user, only: [:index, :show, :edit]
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
